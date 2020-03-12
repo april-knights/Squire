63043e988b1076e854576b43231be36b255b4b33
LeoVerto <LeoVerto@users.noreply.github.com>
2020-03-12 17:29:36 +0100

    Move bootstrap to Laravel Mix workflow, install fontawesome

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,16 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
+ * Load bootstrap
+ */
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/static/css/style.css">
+    <link type="text/css" href="{{ asset('static/css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -54,7 +53,7 @@
         @yield("content")
     </div>
 
-    @yield("script")
+    <script type="text/javascript" src="{{ asset('static/js/app.js') }}"></script>
 </body>
 
 </html>
