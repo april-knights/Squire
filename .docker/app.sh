96cb1b7e15410f6acfe31b0563d08f54358f118b
NorbiPeti <szatmari.norbert.peter@gmail.com>
2022-06-04 03:20:33 +0200

    Create Docker environment for local testing
    - Provided defaults that work out of the box
    - Added MySQL database container
    - Moved LoginController because the autoloader couldn't find it for me

---
+++ .docker/app.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+chown :www-data bootstrap/cache \
+                storage/app/{public,tmp,media} \
+                storage/docs \
+                storage/framework/{cache,sessions,testing,views} \
+                storage/framework/cache/data \
+                storage/logs
+
+# Copy the default environment for first run and make it writable by anyone so that it can be edited
+[ ! -f .env ] && cp .env.default .env && chmod ugo+rw .env
+
+# Generate app key if none is specified
+[[ -z $APP_KEY ]] && php artisan key:generate
+
+# Install packages
+/usr/bin/composer install --no-interaction --prefer-dist
+
+gosu www-data php artisan package:discover --ansi
+
+# Wait for the database to start before running migrations
+wait-for-it squire-database:3306 -t 0 || exit 1
+
+gosu www-data php artisan migrate
+
+exec docker-php-entrypoint "$@"
