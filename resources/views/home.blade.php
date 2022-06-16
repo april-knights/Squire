ecf5d3c9fa9d7a7d409f414de5eb534130103b55
Gryph667 <38962974+Gryph667@users.noreply.github.com>
2022-06-16 04:28:03 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,6 @@
 @section('content')
 <h1>Welcome!</h1>
 <body>You have reached the Squire. This is a work in progress for organizing and displaying Knight matters and concerns in the most organized fashion possible. 
-</br>If, while looking around, you discover something would be nice to have, or needs correcting, please, drop a note about it to <a href="mailto:AprilKnightsDev@gmail.com">AprilKnightsDev@gmail.com</a>.
+</br>If, while looking around, you discover something would be nice to have, or needs correcting, please, drop a note about it to <a href="mailto:AprilKnightsDev@gmail.com? subject=Squire%20Feedback">AprilKnightsDev@gmail.com</a>.
 </body>
 @endsection
