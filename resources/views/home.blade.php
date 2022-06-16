0396abd7a47e54a527dd37b28c1c00a50721cb64
Gryph667 <38962974+Gryph667@users.noreply.github.com>
2022-06-16 04:20:10 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Home')
 
 @section('content')
-<h1>Weecome!</h1>
+<h1>Welcome!</h1>
 <body>You have reached the Squire. This is a work in progress for organizing and displaying Knight matters and concerns in the most organized fashion possible. 
 </br>If, while looking around, you discover something would be nice to have, or needs correcting, please, drop a note about it to <a href="mailto:AprilKnightsDev@gmail.com">AprilKnightsDev@gmail.com</a>.
 </body>
