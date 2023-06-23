16efccc2cdc9e45885edaa1566d316e6bb8517e6
NorbiPeti <szatmari.norbert.peter@gmail.com>
2023-06-23 14:17:17 +0200

    Update to Laravel 10

--- .docker/app.sh
+++ .docker/app.sh
@@ -6,6 +6,9 @@ chown :www-data bootstrap/cache \
                 storage/framework/cache/data \
                 storage/logs
 
+# Wait for the database to start before running migrations or running artisan commands at all
+wait-for-it database:3306 -t 0 || exit 1
+
 # Copy the default environment for first run and make it writable by anyone so that it can be edited
 [ ! -f .env ] && cp .env.default .env && chmod ugo+rw .env && php artisan key:generate
 # Or just set the key if not set yet
@@ -15,9 +18,6 @@ grep APP_KEY=CHANGEME .env > /dev/null && php artisan key:generate
 /usr/bin/composer install --no-interaction --prefer-dist
 
 gosu www-data php artisan package:discover --ansi
-
-# Wait for the database to start before running migrations
-wait-for-it squire-database:3306 -t 0 || exit 1
 
 gosu www-data php artisan migrate
 
