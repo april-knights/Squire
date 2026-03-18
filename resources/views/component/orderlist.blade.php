fd12e5bc29aa119423b368be7f7a9c5d453c36ed
Gryph667 <38962974+Gryph667@users.noreply.github.com>
2026-03-18 15:55:39 -0400

    Update orderlist.blade.php

--- resources/views/component/orderlist.blade.php
+++ resources/views/component/orderlist.blade.php
@@ -1,6 +1,10 @@
 @forelse ($orders as $order)
-    @component('component.order', ['order' => $order])
+    @component('component.order', [
+        'order'          => $order,
+        'can_edit_order' => $can_edit_order ?? false,
+        'can_delete'     => $can_delete ?? false,
+    ])
     @endcomponent
 @empty
-    <p>No orders yet</p>
+    <p>No orders at this time.</p>
 @endforelse
