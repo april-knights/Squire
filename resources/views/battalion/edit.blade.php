31372f5ce70460ee67408206829a842a62443b5d
Gryph667 <38962974+Gryph667@users.noreply.github.com>
2026-03-18 13:58:51 -0400

    Update edit.blade.php

--- resources/views/battalion/edit.blade.php
+++ resources/views/battalion/edit.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
+
 @section('title', 'Edit ' . $batt->name)
-<?php /** @var \App\Model\Battalion $batt */ ?>
 
 @section('content')
+<?php /** @var \App\Model\Battalion $batt */ ?>
 @if ($errors->any())
 <div class="alert alert-danger">
     <ul>
@@ -46,4 +47,84 @@
             <div class="form-group">
                 <label for="battlead">Battalion Leader</label>
                 <select class="form-control" name="battlead">
-                    <option value="" @if(!$batt->battlead) selected @endif>— None —</o
+                    <option value="" @if(!$batt->battlead) selected @endif>— None —</option>
+                    @php /** @var \App\Model\Knight $knight */ @endphp
+                    @foreach ($all_knights as $knight)
+                    <option value="{{ $knight->pkey }}"
+                        @if ($knight->pkey == $batt->battlead) selected @endif>
+                        {{ $knight->rname }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="form-group">
+                <label for="battsec1">First Officer</label>
+                <select class="form-control" name="battsec1">
+                    <option value="" @if(!$batt->battsec1) selected @endif>— None —</option>
+                    @foreach ($all_knights as $knight)
+                    <option value="{{ $knight->pkey }}"
+                        @if ($knight->pkey == $batt->battsec1) selected @endif>
+                        {{ $knight->rname }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="form-group">
+                <label for="battsec2">Second Officer</label>
+                <select class="form-control" name="battsec2">
+                    <option value="" @if(!$batt->battsec2) selected @endif>— None —</option>
+                    @foreach ($all_knights as $knight)
+                    <option value="{{ $knight->pkey }}"
+                        @if ($knight->pkey == $batt->battsec2) selected @endif>
+                        {{ $knight->rname }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label for="motto">Motto</label>
+                <input class="form-control" id="motto" name="motto" type="text"
+                    value="{{ $batt->motto }}" maxlength="64">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label for="battdescr">Description</label>
+                <textarea class="form-control" id="battdescr" name="battdescr"
+                    maxlength="500">{{ $batt->battdescr }}</textarea>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        @if($can_delete)
+        <div class="col">
+            <div class="form-group">
+                <button type="submit" class="btn btn-danger float-left" form="delete"
+                    data-toggle="confirmation"
+                    data-btn-ok-icon-class="fas fa-check"
+                    data-btn-cancel-icon-class="fas fa-ban">Delete Battalion</button>
+            </div>
+        </div>
+        @endif
+        <div class="col">
+            <button type="submit" class="btn btn-success float-right">Save Changes</button>
+        </div>
+    </div>
+</form>
+@if($can_delete)
+<form method="POST" id="delete" action="/battalion/{{ $batt->battalias }}">
+    @csrf
+    @method('DELETE')
+</form>
+@endif
+@endsection
