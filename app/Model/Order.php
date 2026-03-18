1d29a9578b3f16d40c4066b2ff2991f06daaed43
Gryph667 <38962974+Gryph667@users.noreply.github.com>
2026-03-18 16:31:27 -0400

    Update Order.php

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -28,18 +28,4 @@ class Order extends SquireModel {
     public function battalion(): BelongsTo {
         return $this->belongsTo(Battalion::class, 'fkeybattalion', 'pkey');
     }
-}<?php
-
-namespace App\Model;
-
-use App\Support\SquireModel;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
-class Order extends SquireModel {
-    protected $fillable = [
-    ];
-
-    public function author(): BelongsTo {
-        return $this->belongsTo(Knight::class, 'authorid', 'pkey');
-    }
 }
