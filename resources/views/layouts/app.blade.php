ad45db0fabab2450b11d423e187c3284a09fac7f
Gryph667 <38962974+Gryph667@users.noreply.github.com>
2026-03-20 04:52:52 +0000

    Add CSRF meta tag to layout head for AJAX requests

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <title>Squire - @yield('title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link type="text/css" href="{{ asset('static/css/app.css') }}" rel="stylesheet">
 </head>
 
