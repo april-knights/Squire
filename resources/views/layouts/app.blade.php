93e26c4eb62485a24a52b1b7ee71db2ec3cdfde2
Gryph667 <38962974+Gryph667@users.noreply.github.com>
2026-05-18 07:58:42 +0000

    Add warning and info flash message types to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,7 +99,7 @@
                 @else
                 <div class="content col-lg-9">
                 @endif
-                    @if(session()->has('success'))
+                @if(session()->has('success'))
                     <div class="alert alert-success" role="alert">
                         {{ session()->get('success') }}
                     </div>
@@ -107,6 +107,16 @@
                     @if(session()->has('error'))
                     <div class="alert alert-danger" role="alert">
                         {{ session()->get('error') }}
+                    </div>
+                    @endif
+                    @if(session()->has('warning'))
+                    <div class="alert alert-warning" role="alert">
+                        {!! session()->get('warning') !!}
+                    </div>
+                    @endif
+                    @if(session()->has('info'))
+                    <div class="alert alert-info" role="alert">
+                        {{ session()->get('info') }}
                     </div>
                     @endif
                     @yield('content')
